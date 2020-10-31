2d3cd77e44aee313297f186c5bb139b90a26eb96
Janda Baper <64169688+janda09@users.noreply.github.com>
2020-10-31 21:32:44 +0800

    repo

---
+++ repo/info.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#info
+
+clear
+neofetch
+echo -e ""
+echo -e "##############################################"
+echo -e "#              Janda Baper Group             #"
+echo -e "#               Ipang Nett Nott              #"
+echo -e "#            jandabaper09@gmail.com          #"
+echo -e "##############################################"
+echo -e ""
