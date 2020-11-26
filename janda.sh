ffc004502b57febce44d28d6ab062e62ca7112b7
Janda Baper <64169688+janda09@users.noreply.github.com>
2020-11-27 05:36:56 +0700

    Fix

--- janda.sh
+++ janda.sh
@@ -120,8 +120,8 @@ END
 cd
 apt-get -y install dropbear
 sed -i 's/NO_START=1/NO_START=0/g' /etc/default/dropbear
-sed -i 's/DROPBEAR_PORT=22/DROPBEAR_PORT=143/g' /etc/default/dropbear
-sed -i 's/DROPBEAR_EXTRA_ARGS=/DROPBEAR_EXTRA_ARGS="-p 80 -p 109 -p 110 -p 443"/g' /etc/default/dropbear
+sed -i 's/DROPBEAR_PORT=22/DROPBEAR_PORT=443/g' /etc/default/dropbear
+sed -i 's/DROPBEAR_EXTRA_ARGS=/DROPBEAR_EXTRA_ARGS="-p 443 -p 80 -p 109 -p 110 -p 143"/g' /etc/default/dropbear
 
 # update dropbear 2019
 wget https://matt.ucc.asn.au/dropbear/releases/dropbear-2019.78.tar.bz2
